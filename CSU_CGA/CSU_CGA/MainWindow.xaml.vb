36248315880aac05ac9a3db5b1f483d8937ae11c
guimaraes mahota <guimaraesmahota@gmail.com>
2015-02-09 17:04:28 +0200

    Update do layout da pagina inicial

--- CSU_CGA/CSU_CGA/MainWindow.xaml.vb
+++ CSU_CGA/CSU_CGA/MainWindow.xaml.vb
@@ -1,6 +1,8 @@
 ﻿Imports System.IO
 Imports System.Collections.ObjectModel
 Imports CSU_CGA
+Imports System.Globalization
+Imports System.Threading
 
 Class MainWindow
     Public listFiles As ObservableCollection(Of Ficheiro)
@@ -8,27 +10,37 @@ Class MainWindow
 
 
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+
         Dim lista = CType(Me.Resources("ListFicheiros"), Ficheiro_Controller)
 
+        If lista.Count > 0 Then
+            For Each fich As Ficheiro In lista
 
-        'jury_controller = New Jvris_Controller()
-
-        For Each fich As Ficheiro In lista
-
-            If fich.Sel = "True" Then
-                jury_controller.Integracao_Primavera(fich.FicheiroSel)
-            End If
+                If fich.Sel = "True" Then
+                    jury_controller.Integracao_Primavera(fich.FicheiroSel)
+                End If
 
 
-        Next
-        actualizar()
+            Next
+            actualizar()
+        Else
+            MessageBox.Show("Não foi selecionado nenhum ficheiro")
+
+        End If
+        
 
     End Sub
 
     Public Sub New()
+        Thread.CurrentThread.CurrentCulture = New CultureInfo("pt-PT")
+        Thread.CurrentThread.CurrentUICulture = New CultureInfo("pt-PT")
 
+        
         ' This call is required by the designer.
         InitializeComponent()
+
+        dtFim.SelectedDate = Today
+        dtInicio.SelectedDate = New Date(Today.Year, Today.Month, 1)
         Show()
 
         listFiles = New ObservableCollection(Of Ficheiro)()
@@ -46,31 +58,71 @@ Class MainWindow
     End Sub
 
     Private Sub actualizar()
-        Dim caminho As String = ""
-
-        Dim sel As ListBoxItem = cbFicheiro.SelectedValue
-
-        Select Case sel.Content
-            Case "Por Importar" : caminho = "C:\jvris_primavera\Por Importar"
-            Case "Importado" : caminho = "C:\jvris_primavera\Importado"
-            Case "Cancelados" : caminho = "C:\jvris_primavera\Cancelados"
-            Case "Erro de Importação" : caminho = "C:\jvris_primavera\Erro de Importação"
-        End Select
+        Try
+            txtTexto.Text = ""
+            Dim caminho As String = ""
 
 
-        Dim FileNm As DirectoryInfo = New DirectoryInfo(caminho)
-        Dim filename = FileNm.GetFiles()
+            Dim sel As ListBoxItem = cbFicheiro.SelectedValue
 
-        Dim lista = CType(Me.Resources("ListFicheiros"), Ficheiro_Controller)
+            Select Case sel.Content
+                Case "Por Importar" : caminho = "C:\jvris_primavera\Por Importar"
+                Case "Importado" : caminho = "C:\jvris_primavera\Importado"
+                Case "Cancelados" : caminho = "C:\jvris_primavera\Cancelados"
+                Case "Erro de Importação" : caminho = "C:\jvris_primavera\Erro de Importação"
+            End Select
 
-        lista.Clear()
 
-        For Each f As FileInfo In filename
-            lista.Add(
-                New Ficheiro(True, f.ToString(), f)
-            )
-        Next
+            Dim FileNm As DirectoryInfo = New DirectoryInfo(caminho)
+            Dim filename = FileNm.GetFiles()
+
+            Dim lista = CType(Me.Resources("ListFicheiros"), Ficheiro_Controller)
+
+            lista.Clear()
+
+            For Each f As FileInfo In filename
+                lista.Add(
+                    New Ficheiro(True, f.ToString(), f)
+                )
+
+            Next
+
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
 
     End Sub
 
+    Private Sub listViewFolder_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles listViewFolder.SelectionChanged
+        Try
+            Dim lista = CType(Me.Resources("ListFicheiros"), Ficheiro_Controller)
+            Dim objficheiro = lista(listViewFolder.SelectedIndex)
+
+            Dim fileReader As System.IO.StreamReader
+            fileReader = My.Computer.FileSystem.OpenTextFileReader(objficheiro.ficheiro.FullName)
+            txtTexto.Text = fileReader.ReadToEnd()
+            Dim stringReader As String
+
+            stringReader = fileReader.ReadLine()
+
+            Dim palavras As String()
+
+            Dim i As Integer = 1
+
+            While stringReader <> ""
+                palavras = Split(stringReader, ";")
+
+                stringReader = fileReader.ReadLine()
+            End While
+
+            fileReader.Close()
+        Catch ex As Exception
+
+        End Try
+
+
+
+    End Sub
 End Class
